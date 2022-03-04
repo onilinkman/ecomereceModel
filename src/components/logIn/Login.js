b6cbd506ec33a37261dc3d41e789953c9f26ccd8
onilinkman <marbanchristian@gmail.com>
2022-03-03 20:59:38 -0400

    Se esta haciendo la llamada para loguear

--- src/components/logIn/Login.js
+++ src/components/logIn/Login.js
@@ -10,7 +10,49 @@ const themes = {
 };
 
 class Login extends React.Component {
+  state = {
+    loading: false,
+    error: null,
+    data: null,
+  };
+
+  componentDidMount() {}
+
+  fetchData = () => {
+    console.log("hola");
+    this.setState({ loading: true, error: null });
+    var body = JSON.stringify(this.getFormLogin());
+    console.log(body);
+    fetch("http://192.168.0.123:8000/api/v1/users", {
+      method: "POST",
+
+      body: body,
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({ loading: false, data });
+        console.log(data);
+      })
+      .catch((error) => {
+        this.setState({ loading: false, data: error });
+        console.log(error);
+      });
+  };
+
+  getFormLogin = () => {
+    return {
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+    };
+  };
+
   render() {
+    var viewLoading;
+
+    if (this.state.loading) {
+      viewLoading = <div className="loading">Loading...</div>;
+    }
+
     return (
       <div className="Login-container">
         <div className="Login-title">
@@ -26,7 +68,9 @@ class Login extends React.Component {
             <input type="password" id="password" />
           </div>
           <div className="Login-Button">
-            <button className="btn-login">Iniciar Sesión</button>
+            <button className="btn-login" onClick={this.fetchData}>
+              Iniciar Sesión
+            </button>
           </div>
           <div className="Login_Options">
             <p>
@@ -37,6 +81,7 @@ class Login extends React.Component {
             </p>
           </div>
         </div>
+        {viewLoading}
       </div>
     );
   }
